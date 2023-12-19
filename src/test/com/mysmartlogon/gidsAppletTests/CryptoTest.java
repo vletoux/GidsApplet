c8b634eb8a2142d09b4542167eabbbee1800fb56
Rylie Pavlik <rylie.pavlik@collabora.com>
2023-12-19 15:40:45 -0600

    Comment the failing test case

--- src/test/com/mysmartlogon/gidsAppletTests/CryptoTest.java
+++ src/test/com/mysmartlogon/gidsAppletTests/CryptoTest.java
@@ -49,7 +49,8 @@ public class CryptoTest extends GidsBaseTestClass {
         execute("00 E0 00 00 49 62 47 82 01 18 83 02 B0 81 8C 05 8F 10 10 10 00 A5 37 B8 09 80 01 06 83 01 81 95 01 40 B8 09 80 01 86 83 01 81 95 01 40 B8 09 80 01 46 83 01 81 95 01 40 B6 09 80 01 16 83 01 81 95 01 40 B6 09 80 01 56 83 01 81 95 01 40");
         //execute("00 A4 00 0C 02 B0 81");
         execute("00 44 00 00 00");
-        // generate asymmetric key
+        // generate asymmetric key (RSA 1024)
+        // TODO non-standard apdu? Provides no Le.
         execute("00 47 00 00 08 AC 06 80 01 06 83 01 81");
         execute("00 A4 00 0C 02 B0 81");
         // read the public key
